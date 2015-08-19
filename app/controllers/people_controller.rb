5ef099b1902564e881851c837869bd357f92783c
Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2015-08-19 19:13:41 +0300

    People controller and views upgrade

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,14 +1,17 @@
 class PeopleController < ApplicationController
+  before_action :set_person, only: [:show, :edit, :update, :destroy]
 
   def index
   @people = Person.all
   end
-  def show
 
+  def show
   end
+
   def new
     @person = Person.new
   end
+
   def create
     @person = Person.new(person_params)
     if @person.save
@@ -17,6 +20,7 @@ class PeopleController < ApplicationController
       render 'new'
     end
   end
+
   def edit
   end
 
@@ -30,8 +34,15 @@ class PeopleController < ApplicationController
     @person.destroy
     redirect_to people_path
   end
+
   private
+
   def person_params
     params.require(:person).permit(:name, :bio)
   end
+
+  def set_person
+    @person = Person.find(params[:id])
+  end
+
 end
