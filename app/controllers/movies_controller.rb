a97bb04f1f8d481412458ed4032c8218acca8dfd
Erdem <erdem@rails.olcay.org>
2015-08-19 17:29:42 +0300

    Created movies actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,28 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
 end
