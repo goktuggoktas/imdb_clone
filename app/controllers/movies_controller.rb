10d8e89a3a797f4b89f97583e759c2a2b80bcb0b
Erdem <erdem@rails.olcay.org>
2015-08-19 22:13:57 +0300

    Updated movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
+  before_action :set_people, only: [:new, :edit]
 
   def index
     @movies = Movie.all
@@ -12,10 +13,41 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    movie_params = params.require(:movie).permit(:title, :year, :description, :genre_id, :poster)
+    @movie = Movie.new(movie_params)
+
+    if @movie.save then
+      params[:director_ids].each do |director_id|
+        DirectorMovie.create(movie_id: @movie.id, person_id: director_id.to_i)
+      end
+      params[:actor_ids].each do |actor_id|
+        ActorMovie.create(movie_id: @movie.id, person_id: actor_id.to_i)
+      end
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
+  end
+
   def edit
   end
 
   def update
+    movie_params = params.require(:movie).permit(:id, :title, :year, :description, :genre_id, :poster)
+    @movie = Movie.find(params[:id]
+
+    if @movie.update then
+      params[:director_ids].each do |director_id|
+        DirectorMovie.create(movie_id: @movie.id, person_id: director_id.to_i)
+      end
+      params[:actor_ids].each do |actor_id|
+        ActorMovie.create(movie_id: @movie.id, person_id: actor_id.to_i)
+      end
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -24,5 +56,9 @@ class MoviesController < ApplicationController
   private
     def set_movie
       @movie = Movie.find(params[:id])
+    end
+
+    def set_people
+      @people = Person.all
     end
 end
