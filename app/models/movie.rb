18138ed7d3693721a7cabd7b1f763994aad5f6e0
Erdem <erdem@rails.olcay.org>
2015-08-19 17:18:15 +0300

    Created movies controller, added associations to models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+
+  has_many :people, through: :actor_movies
+  has_many :people, through: :director_movies
+  has_one :genre
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,6 @@
 class Person < ActiveRecord::Base
+
+  has_many :movies, through: :actor_movies
+  has_many :movies, through: :director_movies
+
 end
