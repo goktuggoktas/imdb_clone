7e8b45b451b694af4645d8e434b6cfdf8bb8b3bb
Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2015-08-19 17:45:51 +0300

    People controller generated

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,37 @@
+class PeopleController < ApplicationController
+
+  def index
+  @people = Person.all
+  end
+  def show
+
+  end
+  def new
+    @person = Person.new
+  end
+  def create
+    @person = Person.new(person_params)
+    if @person.save
+      redirect_to @person
+    else
+      render 'new'
+    end
+  end
+  def edit
+  end
+
+  def update
+    @person.update(person_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @person = Post.find(params[:id])
+    @person.destroy
+    redirect_to people_path
+  end
+  private
+  def person_params
+    params.require(:person).permit(:name, :bio)
+  end
+end
